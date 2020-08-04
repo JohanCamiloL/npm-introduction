dcf267605051dc7bd54be34a105d161d546a7c4c
Camilo Lopez <johan.lopez@bluetab.net>
2020-08-03 21:01:55 -0500

    Write on file function added

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const coolImages = require('cool-images');
+const moment = require('moment');
+const fs = require('fs');
 
 const randomImage = coolImages.one();
+
+/**
+ * Writes data on file
+ * @param {String} fileName File name where data will be append. 
+ * @param {String} data Data to be written on file.
+ */
+const writeOnFile = (data, fileName = 'log.txt') => {
+    fs.appendFile(fileName, data, (error) => {
+        if (error) console.log('Hubo un error en la escritura');
+        else console.log('Información guardada correctamente');
+    });
+}
 
 /**
  * Get ten images from cool-images module
@@ -8,7 +22,6 @@ const randomImage = coolImages.one();
 const getTenRandomImages = () => {
     const randomImages = coolImages.many(100, 200, 10);
 
-    randomImages.forEach(randomImageURL => console.log(`Image URL: ${randomImageURL}`));
+    randomImages
+        .forEach(randomImageURL => console.log(`Image URL: ${randomImageURL}`));
 }
-
-getTenRandomImages();
